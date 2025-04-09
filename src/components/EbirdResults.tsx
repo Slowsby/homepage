2240ec2dbfa5057ddb3855046bf1eb435d86182e
Slowsby <quentin.peckels@gmail.com>
2025-04-09 12:13:07 +0200

    fix: update .split() to properly handle name and scientific name separation

--- src/components/EbirdResults.tsx
+++ src/components/EbirdResults.tsx
@@ -5,7 +5,7 @@ type Bird = {
 
 export default function RenderBirdResults(birds: Bird[]) {
   return birds.map((el) => {
-    const [name, ...sci] = el.name.split('-');
+    const [name, ...sci] = el.name.split(' -');
     return (
       <a
         className='bg-white p-3 justify-start items-start w-96 hover:bg-gray-300 focus:bg-gray-300 border-black border-1 text-left'
@@ -14,7 +14,7 @@ export default function RenderBirdResults(birds: Bird[]) {
         <div className='text-black'>
           <p>
             <span>{name}</span>
-            <span className='text-gray-600 italic text-sm'>{sci}</span>
+            <span className='text-gray-600 italic text-sm'> {sci}</span>
           </p>
         </div>
       </a>
